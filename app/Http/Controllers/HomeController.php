9d5a777d678c2830f829982226e375f742bbb92f
kulotsystems <kulotsystems@gmail.com>
2022-04-18 16:55:10 +0800

    Installed Laravel 9 and Laravel Breeze.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        $posts = Post::when(request('category_id'), function($query) {
+            $query->where('category_id', request('category_id'));
+        })
+        ->latest()
+        ->get();
+
+        return view('index', compact('categories', 'posts'));
+    }
+}
